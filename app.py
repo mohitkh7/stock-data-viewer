190e8954b10639068a88c84bf5fe0f2322c296c5
Mohit Khandelwal <mohitkh7@gmail.com>
2020-01-05 22:00:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import random
+import string
+import cherrypy
+import redis
+import json
+from scrap import scrap
+
+db = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
+
+global_conf = {
+    "server.socket_port": 8000,
+}
+cherrypy.config.update(global_conf)
+
+class WebService(object):
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def data(self):
+        l = list(db.smembers("data"))
+        for i in range(len(l)):
+            l[i] = json.loads(l[i])
+
+        return json.dumps(l)
+
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def update(self):
+        count = scrap()
+        if count:
+            dic = {
+                "status": 1,
+                "message": "{} stock records fetched and updated.".format(count),
+            }
+        else:
+            dic = {
+                "status": 0,
+                "message": "Failed to update stock data",
+            }
+        return json.dumps(dic)
+        
+    @cherrypy.expose
+    def index(self):
+        return open("./index.html", "r").read()
+
+
+if __name__ == "__main__":
+    conf = {
+        '/': {
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Access-Control-Allow-Origin', '*')],
+        }
+    }
+    cherrypy.quickstart(WebService(), '/', conf)
---
+++ scrap.py
@@ -0,0 +1,51 @@
+from urllib.request import urlopen
+from io import BytesIO
+from zipfile import ZipFile
+import re
+import requests
+import redis
+import json
+
+
+def scrap():
+    print("Fetching Data ...")
+    try:
+        db = redis.Redis()
+        stock_count = 0
+        URL = "https://www.bseindia.com/markets/MarketInfo/BhavCopy.aspx"
+        website = urlopen(URL)
+        if website.status == 200:
+            html = website.read().decode('utf-8')
+            pattern = '"(http://www.bseindia.com/download/BhavCopy/Equity/.*?)"'
+            links = re.findall(pattern, html)
+
+            response = urlopen(links[0])
+
+            zipfile = ZipFile(BytesIO(response.read()))
+            db.delete("data");        
+            for csvfilename in zipfile.namelist():
+                for line in zipfile.open(csvfilename).readlines()[1:]:
+                    arr = line.decode('utf-8').split(",")
+                    dic = {
+                        "CODE": arr[0],
+                        "NAME": arr[1],
+                        "OPEN": arr[4],
+                        "HIGH": arr[5],
+                        "LOW": arr[6],
+                        "CLOSE": arr[7],
+                    }
+                    stock_count += 1
+                    json_string = json.dumps(dic)
+                    db.sadd("data", json_string)
+            print("Data write Completed");
+            return stock_count
+        else:
+            print("Website not reachable")
+            return stock_count
+    except:
+        print("Error while fetching data")
+        return 0
+
+
+if __name__ == "__main__":
+    scrap()
